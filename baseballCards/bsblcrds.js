4c30dfa9c28c10956f98feb0a90ad27661103103
Eric Theobald <10872726@my.uvu.edu>
2021-04-26 18:49:08 -0600

    Began fetch etc.

--- baseballCards/bsblcrds.js
+++ baseballCards/bsblcrds.js
@@ -1,1 +1,26 @@
 //Hello World!
+const pokemonFlex = document.querySelector(".pokemon-flex")
+
+async function getAPIData(url) {
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=25`).then(
+    (data) => {
+        for (const singlePokemon of data.results) {
+            populatePokeCard(singlePokemon)
+        }
+    }
+)
+
+function populatePokeCard(singlePokemon) {
+    let pokeScene = document.createElement("div")
+    let pokeCard = document.createElement("div")
+    console.log(singlePokemon)
+}
