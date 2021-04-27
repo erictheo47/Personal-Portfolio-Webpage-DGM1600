7934c0ee4b9d1fea6ad01a12dd545263a9225323
Eric Theobald <10872726@my.uvu.edu>
2021-04-26 20:47:52 -0600

    added all images implemented 1 image

--- baseballCards/bsblcrds.js
+++ baseballCards/bsblcrds.js
@@ -21,6 +21,45 @@ getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=25`).then(
 
 function populatePokeCard(singlePokemon) {
     let pokeScene = document.createElement("div")
+    pokeScene.className = "scene"
+
     let pokeCard = document.createElement("div")
-    console.log(singlePokemon)
+    pokeCard.className = "card"
+
+    pokeCard.appendChild(populateCardFront(singlePokemon))
+    pokeCard.appendChild(populateCardBack(singlePokemon))
+
+    pokeScene.appendChild(pokeCard)
+    pokemonFlex.appendChild(pokeScene)
+}
+
+function populateCardFront(pokemon) {
+    let pokeFront = document.createElement("div")
+    pokeFront.className = "card__face card__face--front"
+
+    let frontLabel = document.createElement("p")
+    frontLabel.className = "frontLabel"
+    frontLabel.textContent = pokemon.name
+
+    let frontImage = document.createElement("img")
+    frontImage.className = "frontImage"
+    frontImage.src = `images/001.png`
+
+    pokeFront.appendChild(frontImage)
+    pokeFront.appendChild(frontLabel)
+
+    return pokeFront
+}
+
+function populateCardBack(pokemon) {
+    let pokeBack = document.createElement("div")
+    pokeBack.className = "card__face card__face--back"
+
+    let backLabel = document.createElement("p")
+    backLabel.className = "backLabel"
+    backLabel.textContent = `Back of Card`
+
+    pokeBack.appendChild(backLabel)
+
+    return pokeBack
 }
